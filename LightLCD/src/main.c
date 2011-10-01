9c629fe8932a71026128779f99816bf147b54e08
Colin Carter <colin_carter@fmail.co.uk>
2011-10-01 15:18:03 +0100

    Added documentation and removed unused files

--- LightLCD/src/main.c
+++ LightLCD/src/main.c
@@ -3,6 +3,42 @@
  *
  * \brief Display the light sensor reading as a graph
  *
+ * This application uses the ADC to convert the signal from the 
+ * light sensor and displays it as a graph on the LCD.  
+ *
+ * Interrupts are used with the ADC and adc_handler() 
+ * is called when a value is ready. The value is then output to the LCD screen as a pixel 
+ * that has been positioned according to it value.
+ * (0,0) has been moved from its normal top left position to the bottom left
+ * 
+   Once the pixel reaches the right-hand-side of the LCD screen, the display
+   starts again from the left-hand-side.
+ 
+ * ASF Workflow
+   1) First of all I looked for existing examples for the A3BU-XPLAINED board.
+      When I couldn't find any examples I googled for what I was looking for.  The XPLAINED board
+      didn't show any results and also searching for xmega didn't return too many.  As these are
+      new devices I do expect this to change.
+   2) When I couldn't find any existing examples I then checked the source code to the ASF for the
+      board and then the device/service.
+   3) If I found something that was applicable - or even if I didn't - I would then look at the ASF
+      documentation for that device/service.
+   4) The next step would be to look at the datasheet for the peripheral.  Although, as a beginner
+      and not coming from an AVR background, this was often the first step for me.
+      
+  * What would make my ASF design process easier.
+    I am very impressed with the way AVR allows quick access to documentation, whether it's on
+    the web or locally.  I would like an option for when say right click on a ASF function or 
+    structure, it would take me to the API documentation for that function/structure.
+    
+  * What key features would I like added to ASF
+    - I was very interested in the event system of the xmega but I couldn't find any ASF API for it.
+    - It would be nice if some features (even a small subset) could be ported to other AVR devices.
+   
+  * What are my general comments on the ASF
+   - A very professional, well thought out and documented library.  It is nicely integrated into AVR Studio
+     makes it a pleasure to use.
+       
  */
 
 #include <asf.h>
@@ -14,6 +50,12 @@ static struct adc_channel_config adcch_config;
 static volatile uint8_t scaled_light_reading;
 
 static uint8_t x = 0;
+
+static void adc_handler(ADC_t *adc, uint8_t ch_mask, adc_result_t result);
+static void setup_lcd(void);
+static void setup_adc(void);
+static void update_graph_display(void);
+
 
 static void adc_handler(ADC_t *adc, uint8_t ch_mask, adc_result_t result)
 {
@@ -47,7 +89,7 @@ static void setup_adc(void)
     adcch_write_configuration(&ADCA, ADC_CH0, &adcch_config);	
 }
 
-void update_graph_display(void)
+static void update_graph_display(void)
 {
     // Clear just ahead of the pixel position
     gfx_mono_draw_filled_rect(x, 0, 6, GFX_MONO_LCD_HEIGHT, GFX_PIXEL_CLR);
